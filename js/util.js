3a906e3a6be8193a157f580a0f5080e6785a83ea
wixette <wixette@gmail.com>
2012-12-19 15:17:54 +0800

    update requestAnimationFrame function

--- js/util.js
+++ js/util.js
@@ -37,17 +37,44 @@ ppz.util.setElementTransform = function(element, x, y, angle) {
 };
 
 /**
- * Requests an anmiation frame update.
- * @param {function()} callback The function called on the next frame.
+ * Find the browser's version of window.requestAnimationFrame.
+ * @return {function(function())} The browser's version of
+ *     window.requestAnimationFrame (or a setTimeout-based fallback).
+ * @private
+ */
+ppz.util.requestAnimationFrameVariant_ = function() {
+  var versions = ['requestAnimationFrame',
+                  'mozRequestAnimationFrame',
+                  'msRequestAnimationFrame',
+                  'oRequestAnimationFrame',
+                  'webkitRequestAnimationFrame'];
+
+  for (var i = 0; i < versions.length; i++) {
+    var requestAnimationFrame = window[versions[i]];
+
+    // This is bound to window to keep Chrome happy, otherwise it throws
+    // "TypeError: Illegal invocation."
+    if (requestAnimationFrame)
+      return goog.bind(requestAnimationFrame, window);
+  }
+
+  // Must be on an old browser, fall back to setTimeout at 60 FPS.
+  return function(callback) {
+    window.setTimeout(callback, 17);
+  };
+};
+
+/**
+ * Cross-browser access to requestAnimationFrame. Don't rely on the time being
+ * passed to your callback, as that part of the spec is in flux (Date.now() vs.
+ * performance.now()) and the simple fallback below doesn't pass a time at all.
+ * @param {!Function} callback The callback to pass to requestAnimationFrame.
+ * @return {number} A long integer value that uniquely identifies the entry in
+ *     the callback list.
  */
 ppz.util.requestAnimationFrame = function(callback) {
-  var requestFunc = window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.oRequestAnimationFrame ||
-      window.msRequestAnimationFrame ||
-      function(c) {
-        window.setTimeout(c, 17);
-      };
-  requestFunc(callback);
+  ppz.util.requestAnimationFrame =
+      ppz.util.requestAnimationFrameVariant_();
+
+  return ppz.util.requestAnimationFrame(callback);
 };
