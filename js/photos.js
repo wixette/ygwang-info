418cf1cae08469b66e1bebb1b4e6ad8b8ecaf5a1
wixette <wixette@gmail.com>
2012-12-23 00:35:51 +0800

    continue on photos.js

--- js/photos.js
+++ js/photos.js
@@ -8,10 +8,77 @@ goog.require('goog.style');
 goog.require('ppz.util');
 
 /**
+ * The total number of photos.
+ * @type {number}
+ * @const
+ * @private
+ */
+ppz.photos.NUM_ = 38;
+
+/**
+ * The loading text.
+ * @type {string}
+ * @const
+ * @private
+ */
+ppz.photo.LOADING_ = 'Loading...'
+
+/**
+ * The slideshow interval, in milliseconds.
+ * @type {number}
+ * @const
+ * @private
+ */
+ppz.photos.INTERVAL_ = 4000;
+
+/**
+ * The current step of the loading effect.
+ * @type {number}
+ * @private
+ */
+ppz.photos.loadingStep_ = 0;
+
+/**
+ * The time in milliseconds when the last slide is showed.
+ * @type {number}
+ * @private
+ */
+ppz.photos.lastSlideTime_ = 0;
+
+/**
+ * Current photo index.
+ * @type {number}
+ * @private
+ */
+ppz.photos.currentPhoto_ = 0;
+
+/**
+ * The array of image cache, i.e., hidden <img> element.
+ * @type {Array.<Element>}
+ */
+ppz.photos.photoCache_ = new Arrary(ppz.photos.NUM_);
+
+/**
+ * The main loop to load and show photos.
+ */
+ppz.photos.mainLoop() {
+
+  if (ppz.photos.lastSlideTime_ <= 0) {
+    // The loading effects.
+    ppz.photos.loadingStep_;
+  }
+
+  var currentTime = new Date().getTime();
+
+  ppz.util.requestAnimationFrame(ppz.lucky.onFrame_);
+};
+
+/**
  * Inits the slideshow and starts to load photos.
  */
 ppz.photos.init = function() {
-  window.console.log('photos init...');
+  ppz.photos.currentPhoto_ = 0;
+  ppz.photos.mainLoop();
 };
 
 goog.exportSymbol('ppz.photos.init', ppz.photos.init);
