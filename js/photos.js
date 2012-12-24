4880e7c3c7e2f4594632f940229642ec3dae95d1
wixette <wixette@gmail.com>
2012-12-24 10:31:17 +0800

    added the photo loading effect

--- js/photos.js
+++ js/photos.js
@@ -4,6 +4,8 @@
 
 goog.provide('ppz.photos');
 
+goog.require('goog.events');
+goog.require('goog.string');
 goog.require('goog.style');
 goog.require('ppz.util');
 
@@ -21,7 +23,21 @@ ppz.photos.NUM_ = 38;
  * @const
  * @private
  */
-ppz.photo.LOADING_ = 'Loading...'
+ppz.photos.LOADING_ = 'LOADING...';
+
+/**
+ * Max opacity of the loading character.
+ * @type {number}
+ * @private
+ */
+ppz.photos.LOADING_MAX_OPACITY_ = 0.75;
+
+/**
+ * Min opacity of the loading character.
+ * @type {number}
+ * @private
+ */
+ppz.photos.LOADING_MIN_OPACITY_ = 0.25;
 
 /**
  * The slideshow interval, in milliseconds.
@@ -30,6 +46,13 @@ ppz.photo.LOADING_ = 'Loading...'
  * @private
  */
 ppz.photos.INTERVAL_ = 4000;
+
+/**
+ * If the photo is being loaded.
+ * @type {boolean}
+ * @private
+ */
+ppz.photos.loading_ = false;
 
 /**
  * The current step of the loading effect.
@@ -50,35 +73,180 @@ ppz.photos.lastSlideTime_ = 0;
  * @type {number}
  * @private
  */
-ppz.photos.currentPhoto_ = 0;
+ppz.photos.currentPhotoIndex_ = 0;
+
+/**
+ * The order of photos.
+ * @type {Array.<number>}
+ * @private
+ */
+ppz.photos.playlist_ = new Array(ppz.photos.NUM_);
 
 /**
  * The array of image cache, i.e., hidden <img> element.
  * @type {Array.<Element>}
+ * @private
  */
-ppz.photos.photoCache_ = new Arrary(ppz.photos.NUM_);
+ppz.photos.photoCache_ = new Array(ppz.photos.NUM_);
 
 /**
- * The main loop to load and show photos.
+ * The canvas element.
+ * @type {Element}
+ * @private
  */
-ppz.photos.mainLoop() {
+ppz.photos.canvasElem_ = null;
 
-  if (ppz.photos.lastSlideTime_ <= 0) {
-    // The loading effects.
-    ppz.photos.loadingStep_;
+/**
+ * Elements for loading effect.
+ * @type {Array.<Element>}
+ * @private
+ */
+ppz.photos.loadingElems_ = null;
+
+/**
+ * Animation frame count.
+ * @type {number}
+ * @private
+ */
+ppz.photos.frameCount_ = 0;
+
+/**
+ * Updates the loading effect.
+ * @private
+ */
+ppz.photos.updateLoading_ = function() {
+  for (var i = 0; i < ppz.photos.LOADING_.length; i++) {
+    var opacity = ppz.photos.LOADING_MIN_OPACITY_;
+    if (i == ppz.photos.loadingStep_) {
+      opacity = ppz.photos.LOADING_MAX_OPACITY_;
+    }
+    goog.style.showElement(ppz.photos.loadingElems_[i], true);
+    goog.style.setOpacity(ppz.photos.loadingElems_[i], opacity);
   }
+};
 
-  var currentTime = new Date().getTime();
+/**
+ * The main loop to show loading effect.
+ * @private
+ */
+ppz.photos.loadingLoop_ = function() {
+  if (!ppz.photos.loading_) {
+    return;
+  }
+  if (ppz.photos.frameCount_ % 5 == 0) {
+    ppz.photos.updateLoading_();
+    ppz.photos.loadingStep_++;
+    ppz.photos.loadingStep_ %= ppz.photos.LOADING_.length;
+  }
+  ppz.photos.frameCount_++;
+  ppz.util.requestAnimationFrame(ppz.photos.loadingLoop_);
+};
 
-  ppz.util.requestAnimationFrame(ppz.lucky.onFrame_);
+/**
+ * Shows the current photo.
+ * @private
+ */
+ppz.photos.show_ = function() {
+  var number = goog.string.format('%03d', ppz.photos.currentPhotoIndex_);
+  var url = 'images/' + number + '.JPG';
+  window.console.log(url);
+  goog.style.setTransparentBackgroundImage(ppz.photos.canvasElem_, url);
+};
+
+/**
+ * Starts the loading effect.
+ * @private
+ */
+ppz.photos.startLoading_ = function() {
+  if (!ppz.photos.loading_) {
+    ppz.photos.loading_ = true;
+    ppz.photos.frameCount_ = 0;
+    ppz.photos.loadingLoop_();
+  }
+};
+
+/**
+ * Stops the loading effect.
+ * @private
+ */
+ppz.photos.stopLoading_ = function() {
+  if (ppz.photos.loading_) {
+    for (var i = 0; i < ppz.photos.LOADING_.length; i++) {
+      goog.style.showElement(ppz.photos.loadingElems_[i], false);
+    }
+    ppz.photos.loading_ = false;
+  }
+};
+
+/**
+ * Loads the current photo.
+ * @private
+ */
+ppz.photos.load_ = function() {
+  if (ppz.photos.photoCache_[ppz.photos.currentPhotoIndex_]) {
+    // If the cached image already exists, simply set the background of the
+    // canvas.
+    ppz.photos.show_();
+  } else {
+    // Otherwise, loads the image in a hidden img element while showing the
+    // loading effect.
+    ppz.photos.startLoading_();
+    ppz.photos.photoCache_[ppz.photos.currentPhotoIndex_] =
+        document.createElement('img');
+    goog.events.listen(ppz.photos.photoCache_[ppz.photos.currentPhotoIndex_],
+                       'load',
+                       function() {
+                         window.console.log('loaded');
+//                         ppz.photos.stopLoading_();
+//                         ppz.photos.show_();
+                       });
+  }
+};
+
+/**
+ * Suffles the photo list.
+ * @private
+ */
+ppz.photos.shuffle_ = function() {
+  for (var i = 0; i < ppz.photos.NUM_; i++) {
+    ppz.photos.playlist_[i] = i;
+  }
+  for (var i = 0; i < ppz.photos.NUM_ - 2; i++) {
+    var j = Math.floor(Math.random() * (i + 2));
+    var tmp = ppz.photos.playlist_[i];
+    ppz.photos.playlist_[i] = ppz.photos.playlist_[j];
+    ppz.photos.playlist_[j] = tmp;
+  }
 };
 
 /**
  * Inits the slideshow and starts to load photos.
  */
 ppz.photos.init = function() {
-  ppz.photos.currentPhoto_ = 0;
-  ppz.photos.mainLoop();
+  ppz.photos.canvasElem_ = document.getElementById('canvas');
+  ppz.photos.loadingElems_ = new Array(ppz.photos.LOADING_.length);
+  for (var i = 0; i < ppz.photos.LOADING_.length; i++) {
+    var elem = document.createElement('div');
+    var ch = ppz.photos.LOADING_[i];
+    elem.style.position = 'absolute';
+    elem.style.top = '230px';
+    if (ch == 'I' || ch == '.') {
+      elem.style.left = (275 + i * 17 + 5) + 'px';
+    } else {
+      elem.style.left = (275 + i * 17) + 'px';
+    }
+    elem.style.fontFamily = 'verdana,helvetica,arial,sans-serif';
+    elem.style.fontSize = '30px';
+    elem.style.fontWeight = 'bold';
+    elem.style.color = 'white';
+    elem.innerText = ch;
+    goog.style.showElement(elem, false);
+    ppz.photos.canvasElem_.appendChild(elem);
+    ppz.photos.loadingElems_[i] = elem;
+  }
+  ppz.photos.currentPhotoIndex_ = 0;
+  ppz.photos.shuffle_();
+  ppz.photos.load_();
 };
 
 goog.exportSymbol('ppz.photos.init', ppz.photos.init);
